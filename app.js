f2afc17169cb5b993b9e8693d1836466852f39bb
howardi <wakzclin@gmail.com>
2022-04-14 11:56:52 +0100

    javascript

---
+++ app.js
@@ -0,0 +1,22 @@
+
+
+var num1 = prompt("input your first num1");
+var num2 =  prompt("input your last num2");
+var num1 = 25
+var num2 = 30
+var user = 25 + 30
+console.log('the sum of ' + num1 + ' + num2  is ' + user);
+ alert(output);
+ 
+var firstName = prompt("input your first name");
+var lastName =  prompt("input your last name");
+var fullName = firstName.slice(0,4);
+var username = lastName.slice (0,1);
+var user = fullName + username
+var userId = user.toLowerCase();
+ var output = "Your CapacityBay userid is: " + userId;
+ alert(output);
+
+
+
+  
